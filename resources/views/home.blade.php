db533a6d556178cfc32fd2250d59a8fcafcd24be
Corfaya <lauracorrado0696@gmail.com>
2024-09-24 17:03:17 +0200

    [UPD] jumbotron added and styled

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section("content")
+<div class="bg-hero">
+    <button class="btn main-btn text-uppercase fw-bolder text-white rounded-0 position-relative d-none d-md-block">Current Series</button>
+</div>
 <div class="bg-color pt-5">
     <div class="container">
         <div class="row">
@@ -12,8 +15,7 @@
                 <p class="text-uppercase text-white ">{{$comic['series']}}</p>
             </div>
             @endforeach
-            
-            
+
             <div class="col-12">
                 <div class="d-flex justify-content-center my-4">
                     <button class="btn btn-sm text-uppercase fw-bolder text-white rounded-0">Load More</button>
