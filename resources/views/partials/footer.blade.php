0ec7032aa42294f0f542a18f48150a73080aedb3
Corfaya <lauracorrado0696@gmail.com>
2024-09-24 17:34:16 +0200

    [UPD] Footer middle section added

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -33,4 +33,74 @@
             </div>
         </div>
     </div>
+    <div class="footer-middle">
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex py-4 bg-logo">
+                        <div class="me-5">
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">DC Comics</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="">Characters</a></li>
+                                <li><a class="text-decoration-none" href="">Comics</a></li>
+                                <li><a class="text-decoration-none" href="">Movies</a></li>
+                                <li><a class="text-decoration-none" href="">TV</a></li>
+                                <li><a class="text-decoration-none" href="">Games</a></li>
+                                <li><a class="text-decoration-none" href="">Videos</a></li>
+                                <li><a class="text-decoration-none" href="">News</a></li>
+                            </ul>
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">Shop</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="">Shop DC</a></li>
+                                <li><a class="text-decoration-none" href="">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                        <div class="me-5">
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">DC</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="">Terms Of Use</a></li>
+                                <li><a class="text-decoration-none" href="">Privacy policy (New)</a></li>
+                                <li><a class="text-decoration-none" href="">Ad Choices</a></li>
+                                <li><a class="text-decoration-none" href="">Advertising</a></li>
+                                <li><a class="text-decoration-none" href="">Jobs</a></li>
+                                <li><a class="text-decoration-none" href="">Subscriptions</a></li>
+                                <li><a class="text-decoration-none" href="">Talent Workshops</a></li>
+                                <li><a class="text-decoration-none" href="">CPSC Certificates</a></li>
+                                <li><a class="text-decoration-none" href="">Ratings</a></li>
+                                <li><a class="text-decoration-none" href="">Shop Help</a></li>
+                                <li><a class="text-decoration-none" href="">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="me-5">
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">Sites</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none" href="">DC</a></li>
+                                <li><a class="text-decoration-none" href="">MAD Magazine</a></li>
+                                <li><a class="text-decoration-none" href="">DC Kids</a></li>
+                                <li><a class="text-decoration-none" href="">DC Universe</a></li>
+                                <li><a class="text-decoration-none" href="">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    {{-- <div class="bg-color">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="py-4 d-flex justify-content-between">
+                        <button class="btn text-white rounded-0">Sign-up Now!</button>
+                        <div class="d-flex">
+                            <h4 class="text_blue text-uppercase me-4">Follow us</h4>
+                            <a v-for="img, index in socials" :key="index" class="mx-2" href="#"><img :src="img.path" :alt="img.alt"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
 </footer>
