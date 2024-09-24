2a5a1d98ece13867aac6c6377a1bd1bbf1320cd1
Corfaya <lauracorrado0696@gmail.com>
2024-09-24 15:52:48 +0200

    [UPD] header completed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,69 @@
-<header class="pt-3">
+<header class="">
+    <div class="bg-primary mb-4">
+        <div class="container">
+        
+            <div class="row">
+                <div class="col-12">
+                    <ul class="d-flex justify-content-end list-unstyled text-white fw-bold py-2 m-0">
+                        <li class="text-uppercase">DC Power&#8480; Visa&reg;</li>
+                        <li class="text-uppercase nav-item dropdown ms-5">
+                            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="#">Additional Dc Sites</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Item</a></li>
+                                <li><a class="dropdown-item" href="#">Another item</a></li>
+                              </ul>
+                        </li>
+                    </ul>
+    
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <div class="d-flex justify-content-between align-items-center">
-                    <a href="#"><img class="img-fluid" src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC Comics"></a>
-                    <ul class="list-unstyled d-flex">
+                    <a href="{{route('homepage')}}"><img class="img-fluid" src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC Comics"></a>
+                    <ul class="list-unstyled d-none d-lg-flex m-0">
                         <li class="px-2 fw-bolder">
-                            <a class="text-decoration-none text-uppercase" href="#"> ITEM </a>
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Characters</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Comics</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Movies</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Tv</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Games</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Collectibles</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Videos</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">Fans</a>
+                        </li>
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase text-dark" href="">News</a>
+                        </li>
+                        <li class="px-2 fw-bolder dropdown">
+                            <a class="text-decoration-none text-uppercase text-dark dropdown-toggle" role="button" data-bs-toggle="dropdown" href="">Shop</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Item</a></li>
+                                <li><a class="dropdown-item" href="#">Another item</a></li>
+                              </ul>
                         </li>
                     </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control border-top-0 border-start-0 border-end-0 text-end" type="search" placeholder="Search" aria-label="Search">
+                        <a href="#"><i class="bi bi-search text-dark"></i></a>
+                      </form>
                 </div>
             </div>
         </div>
