760ab4ea970ea10efdf84b78d6d0518d66a19dad
Corfaya <lauracorrado0696@gmail.com>
2024-09-24 19:21:49 +0200

    [UPD] favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="{{Vite::asset('resources/img/favicon.ico')}}" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel DC comics</title>
     {{-- Bootstrap --}}
