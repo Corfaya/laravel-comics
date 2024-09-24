93313b96f19934004b34b25b02998ad51a253ebf
Corfaya <lauracorrado0696@gmail.com>
2024-09-24 15:13:24 +0200

    [UPD] include method for header and footer. Home.blade.php created

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header class="pt-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="#"><img class="img-fluid" src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC Comics"></a>
+                    <ul class="list-unstyled d-flex">
+                        <li class="px-2 fw-bolder">
+                            <a class="text-decoration-none text-uppercase" href="#"> ITEM </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
