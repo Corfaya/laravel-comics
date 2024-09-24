20b10f7c0088e61b26477187e9e62039f42d42f6
Corfaya <lauracorrado0696@gmail.com>
2024-09-24 19:14:40 +0200

    [FIX] blue section moved to homepage/comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,4 +24,39 @@
         </div>
     </div>
 </div>
+
+<div class="banner py-5">
+    <div class="d-flex align-items-center justify-content-around blue-section">
+        <div class="d-flex align-items-center">
+            <a class="w_square me-3 d-flex align-items-center" href="">
+                <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="">
+            </a>
+            <p class="d-none d-lg-block m-0 text-uppercase text-white">Digital Comics</p>
+        </div>
+        <div class="d-flex align-items-center">
+            <a class="w_square me-3 d-flex align-items-center" href="">
+                <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="">
+            </a>
+            <p class="d-none d-lg-block m-0 text-uppercase text-white">Dc Merchandise</p>
+        </div>
+        <div class="d-flex align-items-center">
+            <a class="w_square me-3 d-flex align-items-center" href="">
+                <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="">
+            </a>
+            <p class="d-none d-lg-block m-0 text-uppercase text-white">Subscription</p>
+        </div>
+        <div class="d-flex align-items-center">
+            <a class="w_square me-3 d-flex align-items-center" href="">
+                <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="">
+            </a>
+            <p class="d-none d-lg-block m-0 text-uppercase text-white">Comic Shop Locator</p>
+        </div>
+        <div class="d-flex align-items-center">
+            <a class="w_square me-3 d-flex align-items-center" href="">
+                <img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="">
+            </a>
+            <p class="d-none d-lg-block m-0 text-uppercase text-white">Dc Power Visa</p>
+        </div>
+    </div>
+</div>
 @endsection
